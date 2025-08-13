7f40f4ec0c152adb0e9ee724b83edf83167f5f55
Tien Chau <99374975+gotecq-tienchau@users.noreply.github.com>
2025-08-13 17:51:16 +0700

    chore: update import path

--- src/__test__/conftest.py
+++ src/__test__/conftest.py
@@ -4,7 +4,7 @@ import pytest
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
-from core.web_driver.driver_factory import DriverFactory
+from src.core.web_driver.driver_factory import DriverFactory
 from src.model.const import DEFAULT_WAIT_TIME_OUT
 
 initial_url = os.getenv("BASE_URL")
